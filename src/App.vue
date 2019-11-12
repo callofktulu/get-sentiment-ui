01d9879d59e3cd4dd51171baf52880f34ac76d6f
tu <tu@pop-os.localdomain>
2019-11-12 19:23:35 +0100

    vuetify table for single sentiment unit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link href="/home">
           <v-list-item-action>
@@ -32,49 +29,51 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="blue"
-      dark
-    >
+    <v-app-bar app color="blue" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Sentiment Analyzer</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col class="col-4">
-              <v-card>
-                <v-form>
-                  <v-text-field v-model="word" placeholder="Type the word here" class="ma-3"/>
-                  <v-spacer></v-spacer>
-                  <v-btn 
-                    color="success" 
-                    class="ma-3 justify-right"
-                    v-on:click="getSentiment(word)">Analyze</v-btn>
-                </v-form>
-              </v-card>
+            <v-card>
+              <v-form>
+                <v-text-field v-model="word" placeholder="Type the word here" class="ma-3" />
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="success"
+                  class="ma-3 justify-right"
+                  v-on:click="getSentiment(word)"
+                >Analyze</v-btn>
+              </v-form>
+            </v-card>
           </v-col>
           <v-col class="col-8">
-
-              {{ sentiment }}
-
+            <v-simple-table v-if="sentiment">
+              <tr>
+                <th>Unit</th>
+                <th>Positivity</th>
+                <th>Negativity</th>
+                <th>Objectivity</th>
+                <th>Polarity</th>
+                <th>Dictionary</th>
+              </tr>
+              <tr>
+                <td>{{ sentiment.data.unit }}</td>
+                <td>{{ sentiment.data.positivity }}</td>
+                <td>{{ sentiment.data.negativity }}</td>
+                <td>{{ sentiment.data.objectivity }}</td>
+                <td>{{ sentiment.data.polarity }}</td>
+                <td>{{ sentiment.data.dictionary }}</td>
+              </tr>
+            </v-simple-table>
           </v-col>
         </v-row>
       </v-container>
-
     </v-content>
-    <v-footer
-      color="blue"
-      app
-    >
+    <v-footer color="blue" app>
       <span class="white--text">&copy; 2019 Tuğrul Ertürk</span>
     </v-footer>
   </v-app>
@@ -83,24 +82,25 @@
 <script>
 // import HelloWorld from './components/HelloWorld';
 // import HelloWorld from './components/SingleUnit';
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   props: {
-      source: String,
-      word: String
-    },
+    source: String,
+    word: String,
+    sentiment: []
+  },
   methods: {
-    getSentiment(){
+    getSentiment() {
       axios
-        .get('http://127.0.0.1:8000/sentiment/' + this.word)
-        .then(response => (this.sentiment = response))
+        .get("http://127.0.0.1:8000/sentiment/" + this.word)
+        .then(response => (this.sentiment = response));
     }
   },
   components: {
     // HelloWorld,
-    // SingleUnit,    
+    // SingleUnit,
   },
   data: () => ({
     drawer: null,
